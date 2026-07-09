5bdd01f2729c54e3a39ec22856976c97e42ad9b7
Anoop Rajoriya <anooprajoriya9827@gmail.com>
2026-07-09 20:49:20 +0530

    feat: solved 10-tip-calculator

--- chaicode-webdev-2026/js-conditionals-anoop-rajoriya/src/10-tip-calculator.js
+++ chaicode-webdev-2026/js-conditionals-anoop-rajoriya/src/10-tip-calculator.js
@@ -31,4 +31,38 @@
  */
 export function calculateTip(billAmount, serviceRating) {
   // Your code here
+  if (typeof billAmount !== "number" || typeof serviceRating !== "number")
+    return null;
+
+  if (
+    billAmount <= 0 ||
+    !Number.isInteger(serviceRating) ||
+    serviceRating <= 0 ||
+    serviceRating > 5
+  )
+    return null;
+
+  let tipPercentage = 0;
+
+  switch (serviceRating) {
+    case 1:
+      tipPercentage = 5;
+      break;
+    case 2:
+      tipPercentage = 10;
+      break;
+    case 3:
+      tipPercentage = 15;
+      break;
+    case 4:
+      tipPercentage = 20;
+      break;
+    case 5:
+      tipPercentage = 25;
+  }
+
+  const tipAmount = Number(((billAmount / 100) * tipPercentage).toFixed(2));
+  const totalAmount = Number((billAmount + tipAmount).toFixed(2));
+
+  return { tipPercentage, tipAmount, totalAmount };
 }
